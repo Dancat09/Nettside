59f6a89b151317ba3e11ba63db4f08d6cef6ea64
Dancat09 <davoa006@osloskolen.no>
2026-08-27 14:25:08 +0200

    Main commit

--- script.js
+++ script.js
@@ -16,4 +16,36 @@
             document.body.setAttribute("data-theme", newTheme);
             localStorage.setItem("theme", newTheme);
         }) 
-        
+
+let count = 0;
+
+function updateCount() {
+  document.getElementById("count").innerHTML = count;
+}
+
+function increaseCount() {
+  count++;
+  updateCount();
+}
+
+function decreaseCount() {
+  count--;
+  updateCount();
+}
+
+function resetCount() {
+  count = 0;
+  updateCount();
+}
+
+function saveCount() {
+  localStorage.setItem("count", count);
+}
+
+function loadCount() {
+  let saved = localStorage.getItem("count");
+  if (saved !== null) {
+    count = Number(saved);
+  }
+  updateCount();
+} 
